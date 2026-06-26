02b506099dbf81aa4b9bffd5d1ca603782a172f4
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-26 16:51:09 -0400

    cached list values

---
+++ script.js
@@ -0,0 +1,9 @@
+// CACHE VALUES
+const todoList = document.getElementById("todo-list");
+const inProgressList = document.getElementById("inprogress-list");
+const completeList = document.getElementById("complete-list");
+const overdueList = document.getElementById("overdue-list");
+
+// EVENT LISTENERS
+
+// FUNCTIONS
