27d421a712425574ca18d938443ef1529c767d68
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-26 16:54:46 -0400

    cached form input values

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // CACHE VALUES
+const taskName = document.getElementById("task-name");
+const taskDueDate = document.getElementById("task-date");
+const taskStatus = document.getElementById("task-status");
 const todoList = document.getElementById("todo-list");
 const inProgressList = document.getElementById("inprogress-list");
 const completeList = document.getElementById("complete-list");
 const overdueList = document.getElementById("overdue-list");
+const addBtn = document.getElementById("add-btn");
 
 // EVENT LISTENERS
+addBtn.addEventListener("click", addTask);
 
 // FUNCTIONS
+function addTask() {
+    
+}
