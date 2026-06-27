f3f93ea62fa42ee2da4730f40f55c92b25cf246e
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-27 17:37:17 -0400

    created display tasks function to map through tasks array and show each task

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ addBtn.addEventListener("click", addTask);
 function addTask(e) {
     e.preventDefault();
 
+    console.log('button clicked!');
+
     let title = taskName.value;
     let dueDate = taskDueDate.value;
     let status = taskStatus.value;
@@ -23,10 +25,39 @@ function addTask(e) {
     tasks.push({
         name: title.charAt(0).toUpperCase() + title.slice(1),
         date: dueDate,
-        status: "To Do"
+        status: status
     });
 
     taskName.value = "";
     taskDueDate.value = "";
     taskStatus.value = "To Do";
+
+    displayTasks();
+    console.log(tasks);
+}
+
+function displayTasks() {
+    todoList.innerHTML = "";
+    inProgressList.innerHTML = "";
+    completeList.innerHTML = "";
+
+    tasks.map(task => {
+        const taskItem = document.createElement("li");
+
+        taskItem.innerHTML = `
+        <p>${task.name}</p>
+        <div class="card-bottom">
+            <span>${task.date}</span>
+            <span>${task.status}</span>
+        </div>
+        `;
+
+        if (task.status === "To Do") {
+            todoList.appendChild(taskItem);
+        } else if (task.status === "In Progress") {
+            inProgressList.appendChild(taskItem);
+        } else {
+            completeList.appendChild(taskItem);
+        }
+    });
 }
