d742ce874b1dd3788b7f44be8a2f7bc730f93435
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-27 17:17:13 -0400

    created addTask function

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // CACHE VALUES
-const taskName = document.getElementById("task-name");
-const taskDueDate = document.getElementById("task-date");
-const taskStatus = document.getElementById("task-status");
-const todoList = document.getElementById("todo-list");
-const inProgressList = document.getElementById("inprogress-list");
-const completeList = document.getElementById("complete-list");
-const overdueList = document.getElementById("overdue-list");
-const addBtn = document.getElementById("add-btn");
+let taskName = document.getElementById("task-name");
+let taskDueDate = document.getElementById("task-date");
+let taskStatus = document.getElementById("task-status");
+let todoList = document.getElementById("todo-list");
+let inProgressList = document.getElementById("inprogress-list");
+let completeList = document.getElementById("complete-list");
+let overdueList = document.getElementById("overdue-list");
+let addBtn = document.getElementById("add-btn");
+let tasks = [];
 
 // EVENT LISTENERS
 addBtn.addEventListener("click", addTask);
 
 // FUNCTIONS
-function addTask() {
-    
+function addTask(e) {
+    e.preventDefault();
+
+    let title = taskName.value;
+    let dueDate = taskDueDate.value;
+    let status = taskStatus.value;
+
+    tasks.push({
+        name: title.charAt(0).toUpperCase() + title.slice(1),
+        date: dueDate,
+        status: "To Do"
+    });
+
+    taskName.value = "";
+    taskDueDate.value = "";
+    taskStatus.value = "To Do";
 }
